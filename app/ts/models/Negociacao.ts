89709dca43f00a25059257c4774154353189ac86
hemanuelx <hemanuelx@gmail.com>
2020-05-24 16:48:41 -0300

    Modularization

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,17 +1,5 @@
-class Negociacao {
-    constructor (private _data: Date, private _quantidade: number, private _valor: number) {}
-
-    get data() {
-        return this._data;
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
+export class Negociacao {
+    constructor (readonly _data: Date, readonly _quantidade: number, readonly _valor: number) {}
 
     get volume() {
         return this._quantidade * this._valor;
